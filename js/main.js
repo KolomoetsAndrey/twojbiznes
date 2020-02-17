5eb0e7d313b904f7f28f5af9b6522e0636c4855c
Andrey Kolomoets <kolomoets.and@gmail.com>
2020-02-17 18:03:30 +0200

    index

--- js/main.js
+++ js/main.js
@@ -1,3 +1,12 @@
 $('.carousel').carousel({
     interval: 200000
 });
+
+$(function(){
+    $("#step-one").percircle({
+        text: "01",
+        percent: 20,
+        progressBarColor: "#F6155E",
+        animate: "true"
+    });
+});
