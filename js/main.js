f0d833000ee557daa4334176ea71dbb331928a5c
Andrey Kolomoets <kolomoets.and@gmail.com>
2020-02-19 15:07:12 +0200

    map

--- js/main.js
+++ js/main.js
@@ -18,12 +18,3 @@ $(document).ready(function(){
       });
     }
 });
-
-function initMap() {
-    var uluru = {lat: -25.344, lng: 131.036};
-
-  var map = new google.maps.Map(document.getElementById('map'), {
-    zoom: 4,
-    center: uluru
-  });
-};
