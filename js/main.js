a78dccbba4b3cc9b63d31f31e2c375b8f4cf1618
Andrey Kolomoets <kolomoets.and@gmail.com>
2020-03-10 18:51:05 +0200

    to top

--- js/main.js
+++ js/main.js
@@ -191,3 +191,20 @@ $('.header_menu_link').click( function(){
 function closeNav() {
   document.getElementById("mySidepanel").style.width = "0";
 };
+
+window.onscroll = function() {
+  scrollFunction()
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+  document.getElementById("toTop").style.display = "block";
+  } else {
+  document.getElementById("toTop").style.display = "none";
+  }
+}
+
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
