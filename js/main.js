4fb7bed3ad3a6273b5893f4f602efa80cf5f873a
Andrey Kolomoets <kolomoets.and@gmail.com>
2020-03-10 12:50:28 +0200

    desktop fix

--- js/main.js
+++ js/main.js
@@ -2,6 +2,160 @@ $('.carousel').carousel({
     interval: 5000
 });
 
+$(document).ready(function() {
+	$('.provide_block').addClass("hidden").viewportChecker({
+      classToAdd: 'visible animated fadeInLeft',
+      classToRemove: 'hidden',
+      offset: 100
+  });
+  $('.provide_img_block').addClass("hidden").viewportChecker({
+      classToAdd: 'visible animated fadeInRight',
+      classToRemove: 'hidden',
+      offset: 100
+  });
+  $('.best_left').addClass("hidden").viewportChecker({
+      classToAdd: 'visible animated fadeInLeft',
+      classToRemove: 'hidden',
+      offset: 100
+  });
+  $('.best_right').addClass("hidden").viewportChecker({
+      classToAdd: 'visible animated fadeInRight',
+      classToRemove: 'hidden',
+      offset: 100
+  });
+  $('.best_up').addClass("hidden").viewportChecker({
+      classToAdd: 'visible animated fadeInUp',
+      classToRemove: 'hidden',
+      offset: 100
+  });
+  $('.consultation').addClass("hidden").viewportChecker({
+      classToAdd: 'visible animated fadeInLeft',
+      classToRemove: 'hidden',
+      offset: 100
+  });
+  $('.best_title').addClass("hidden").viewportChecker({
+      classToAdd: 'visible animated fadeIn',
+      classToRemove: 'hidden',
+      offset: 100
+  });
+  $('.work_one').addClass("hidden").viewportChecker({
+      classToAdd: 'visible animated fadeInRight'
+  });
+  $('.work_two').addClass("hidden").viewportChecker({
+      classToAdd: 'visible animated fadeInRight'
+  });
+  $('.work_three').addClass("hidden").viewportChecker({
+      classToAdd: 'visible animated fadeInRight'
+  });
+  $('.work_four').addClass("hidden").viewportChecker({
+      classToAdd: 'visible animated fadeInRight'
+  });
+  $('.work_five').addClass("hidden").viewportChecker({
+      classToAdd: 'visible animated fadeInLeft delay-1s'
+  });
+  $('.people').addClass("hidden").viewportChecker({
+      classToAdd: 'visible animated fadeIn',
+      callbackFunction: function() {
+        var
+            count = 1,
+            block = $( '.people' ),
+            interval = setInterval( function () {
+              count++;
+              block.text( count );
+              if( count === 270 ) {
+                clearInterval( interval );
+              }
+            }, 1 );
+          block.text( count );
+      }
+  });
+  $('.busines').addClass("hidden").viewportChecker({
+    classToAdd: 'visible animated fadeIn',
+    callbackFunction: function() {
+      var
+          count = 1,
+          block = $( '.busines' ),
+          interval = setInterval( function () {
+            count++;
+            block.text( count );
+            if( count === 150 ) {
+              clearInterval( interval );
+            }
+          }, 1 );
+        block.text( count );
+      }
+  });
+  $('.language').addClass("hidden").viewportChecker({
+    classToAdd: 'visible animated fadeIn',
+    callbackFunction: function() {
+      var
+          count = 456,
+          block = $( '.language' ),
+          interval = setInterval( function () {
+            count++;
+            block.text( count );
+            if( count === 740 ) {
+              clearInterval( interval );
+            }
+          }, 1 );
+        block.text( count );
+      }
+  });
+  $('.houses').addClass("hidden").viewportChecker({
+    classToAdd: 'visible animated fadeIn',
+    callbackFunction: function() {
+      var
+          count = 1,
+          block = $( '.houses' ),
+          interval = setInterval( function () {
+            count++;
+            block.text( count );
+            if( count === 130 ) {
+              clearInterval( interval );
+            }
+          }, 1 );
+        block.text( count );
+      }
+  });
+  $('.collapsed').addClass("hidden").viewportChecker({
+      classToAdd: 'visible animated fadeInUp'
+  });
+  $('.form_faq').addClass("hidden").viewportChecker({
+      classToAdd: 'visible animated fadeIn'
+  });
+  $('#testimonialCarousel .carousel-item').addClass("hidden").viewportChecker({
+      classToAdd: 'visible animated fadeIn',
+      classToRemove: 'hidden',
+      offset: 10
+  });
+  $('.people_img').addClass("hidden").viewportChecker({
+      classToAdd: 'visible animated fadeInUp',
+      classToRemove: 'hidden',
+      offset: 10
+  });
+  $('.contact_block').addClass("hidden").viewportChecker({
+      classToAdd: 'visible animated fadeInUp'
+  });
+  $('.form_contact').addClass("hidden").viewportChecker({
+      classToAdd: 'visible animated fadeIn'
+  });
+  $('.map').addClass("hidden").viewportChecker({
+      classToAdd: 'visible animated fadeInUp'
+  });
+});
+
+
+$('#main_carousel').find('.carousel-item').each(function (index, item) {
+  var carouselItem = $(item).find('.carousel-number');
+
+  if (carouselItem.length > 0) {
+    carouselItem.text(index + 1);
+  }
+});
+
+var slidesLength = $('#main_carousel').find('.carousel-item').length;
+$('.last-number').text(slidesLength);
+
 $(document).ready(function(){
     var coll = document.getElementsByClassName("collapsible");
     var i;
@@ -29,6 +183,10 @@ function openNav() {
       document.getElementById("mySidepanel").style.width = "35%";
   }
 };
+
+$('.header_menu_link').click( function(){
+  document.getElementById("mySidepanel").style.width = "0";
+});
   
 function closeNav() {
   document.getElementById("mySidepanel").style.width = "0";
